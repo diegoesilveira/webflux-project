fb09bd76bb50e3aafdad84d2a460200cd027ca8b
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2020-06-28 08:50:55 -0300

    Configuração inicial do projeto com Spring WebFlux.

---
+++ WebFlux-Project/src/main/java/br/com/flux/WebFluxProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxProjectApplication.class, args);
+	}
+
+}
